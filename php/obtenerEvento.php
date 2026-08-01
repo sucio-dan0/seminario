a819d0d24714b1df1ed78e6b96543074586bb87d
sucio-dan0 <trejodelvallemarcoantonio@gmail.com>
2026-07-31 22:46:11 -0600

    Update obtenerEvento.php

--- php/obtenerEvento.php
+++ php/obtenerEvento.php
@@ -1,79 +1,31 @@
 <?php
-
 require_once("conexion.php");
-
 header("Content-Type: application/json; charset=utf-8");
-
-
 if(!isset($_GET["id"])){
-
     echo json_encode([
         "error"=>"No llegó el ID"
     ]);
-
     exit;
-
 }
-
-
-
 $id = $_GET["id"];
-
-
-
 $sql = "SELECT * FROM eventos WHERE idevento = ?";
-
-
-
 $stmt = $conexion->prepare($sql);
-
-
-
 if(!$stmt){
-
     echo json_encode([
         "error"=>"Error preparando consulta: ".$conexion->error
     ]);
-
     exit;
-
 }
-
-
-
 $stmt->bind_param("i",$id);
-
-
-
 $stmt->execute();
-
-
-
 $resultado = $stmt->get_result();
-
-
-
 if($evento = $resultado->fetch_assoc()){
-
-
     echo json_encode($evento);
-
-
-
 }else{
-
-
     echo json_encode([
         "error"=>"No existe evento con id ".$id
-    ]);
-
+                     ]);
 }
-
-
-
 $stmt->close();
-
 $conexion->close();
-
-
 ?>
